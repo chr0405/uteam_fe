999e36b5dca2edb23e7c96ec0fd0fb28de3da346
chr0405 <ch0nn@naver.com>
2024-01-05 07:24:44 +0900

    AM0724

--- react-folder/src/Component/DetailsPage/Contract.jsx
+++ react-folder/src/Component/DetailsPage/Contract.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import SearchAndWrite from './SearchAndWrite'
+import { Link } from 'react-router-dom'
+import { FaHeart } from "react-icons/fa";
 
 export default function Contract() {
   return (
-    <div>계약
-      <SearchAndWrite/>
+    <div>
+      <div>
+        <p>계약</p>
+        <SearchAndWrite/>
+      </div>
+      <ul>
+          <li>
+              <span><Link to="/ContractDummy">전세 사기를 예방하기 위해서 계약 시 넣어야 할 특약 5가지</Link></span>
+              <div>
+              <FaHeart size="20"/>
+                <p>82</p>
+              </div>
+              <p>지구</p>
+          </li>
+      </ul>
     </div>
+    
   )
 }
--- react-folder/src/Component/DetailsPage/Cook.jsx
+++ react-folder/src/Component/DetailsPage/Cook.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import SearchAndWrite from './SearchAndWrite'
 
+import { Link } from 'react-router-dom'
+import { FaHeart } from "react-icons/fa";
+
 export default function Cook() {
   return (
-    <div>요리
+    <div>
+      <div>
+      <p>요리</p>
       <SearchAndWrite/>
     </div>
+      <ul>
+        <li>
+            <span><Link to="/CookDummy">간단하게 카레 만드는 법</Link></span>
+            <div>
+            <FaHeart size="20"/>
+              <p>4566</p>
+            </div>
+            <p>ggg111</p>
+        </li>
+      </ul>
+    </div>
+    
   )
 }
